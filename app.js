f054617a3a0624ad21c6c17a607037151d2b1551
DrDuxx <elias_lichaa@outlook.com>
2022-06-19 03:09:48 +0300

    m

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+app.get("/json/:jsonNumber", async (req, res) => {
+  try {
+    const { jsonNumber } = req.params;
+    if (jsonNumber < 0 || jsonNumber > 6000)
+      return res.status(404).json({ message: "Not found" });
+    const json = await fs.readFileSync(`./json/${jsonNumber}.json`);
+    return res.status(200).send(json);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Connected on port ${PORT}`);
+});
