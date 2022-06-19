fdb912bd8814c5788570457dc4dcd004fc62063a
DrDuxx <elias_lichaa@outlook.com>
2022-06-19 03:19:05 +0300

    m

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ app.get("/json/:jsonNumber", async (req, res) => {
     if (jsonNumber < 0 || jsonNumber > 6000)
       return res.status(404).json({ message: "Not found" });
     const json = await fs.readFileSync(`./json/${jsonNumber}.json`);
-    return res.status(200).json(JSON.stringify(json));
+    const jsonObject = JSON.parse(json)
+    return res.status(200).json(jsonObject);
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Something went wrong" });
